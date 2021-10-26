1013223f88731d705c2a521156915909722256ca
navrotskyj <navrotskyj@gmail.com>
2021-10-26 16:35:59 +0300

    WTEL-2143

--- engine/member.pb.go
+++ engine/member.pb.go
@@ -7,11 +7,11 @@ import (
 	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
-	_struct "github.com/golang/protobuf/ptypes/struct"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	structpb "google.golang.org/protobuf/types/known/structpb"
 	math "math"
 )
 
@@ -1649,27 +1649,27 @@ func (m *SearchMemberAttemptsRequest) GetDomainId() int64 {
 }
 
 type MemberAttempt struct {
-	Id                   int64          `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
-	Member               *Lookup        `protobuf:"bytes,2,opt,name=member,proto3" json:"member,omitempty"`
-	CreatedAt            int64          `protobuf:"varint,3,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
-	Destination          string         `protobuf:"bytes,4,opt,name=destination,proto3" json:"destination,omitempty"`
-	Weight               int32          `protobuf:"varint,5,opt,name=weight,proto3" json:"weight,omitempty"`
-	OriginateAt          int64          `protobuf:"varint,6,opt,name=originate_at,json=originateAt,proto3" json:"originate_at,omitempty"`
-	AnsweredAt           int64          `protobuf:"varint,7,opt,name=answered_at,json=answeredAt,proto3" json:"answered_at,omitempty"`
-	BridgedAt            int64          `protobuf:"varint,8,opt,name=bridged_at,json=bridgedAt,proto3" json:"bridged_at,omitempty"`
-	HangupAt             int64          `protobuf:"varint,9,opt,name=hangup_at,json=hangupAt,proto3" json:"hangup_at,omitempty"`
-	Resource             *Lookup        `protobuf:"bytes,10,opt,name=resource,proto3" json:"resource,omitempty"`
-	LegAId               string         `protobuf:"bytes,11,opt,name=leg_a_id,json=legAId,proto3" json:"leg_a_id,omitempty"`
-	LegBId               string         `protobuf:"bytes,12,opt,name=leg_b_id,json=legBId,proto3" json:"leg_b_id,omitempty"`
-	Node                 string         `protobuf:"bytes,13,opt,name=node,proto3" json:"node,omitempty"`
-	Result               string         `protobuf:"bytes,14,opt,name=result,proto3" json:"result,omitempty"`
-	Agent                *Lookup        `protobuf:"bytes,15,opt,name=agent,proto3" json:"agent,omitempty"`
-	Bucket               *Lookup        `protobuf:"bytes,16,opt,name=bucket,proto3" json:"bucket,omitempty"`
-	Logs                 *_struct.Value `protobuf:"bytes,17,opt,name=logs,proto3" json:"logs,omitempty"`
-	Active               bool           `protobuf:"varint,19,opt,name=active,proto3" json:"active,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
-	XXX_unrecognized     []byte         `json:"-"`
-	XXX_sizecache        int32          `json:"-"`
+	Id                   int64           `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	Member               *Lookup         `protobuf:"bytes,2,opt,name=member,proto3" json:"member,omitempty"`
+	CreatedAt            int64           `protobuf:"varint,3,opt,name=created_at,json=createdAt,proto3" json:"created_at,omitempty"`
+	Destination          string          `protobuf:"bytes,4,opt,name=destination,proto3" json:"destination,omitempty"`
+	Weight               int32           `protobuf:"varint,5,opt,name=weight,proto3" json:"weight,omitempty"`
+	OriginateAt          int64           `protobuf:"varint,6,opt,name=originate_at,json=originateAt,proto3" json:"originate_at,omitempty"`
+	AnsweredAt           int64           `protobuf:"varint,7,opt,name=answered_at,json=answeredAt,proto3" json:"answered_at,omitempty"`
+	BridgedAt            int64           `protobuf:"varint,8,opt,name=bridged_at,json=bridgedAt,proto3" json:"bridged_at,omitempty"`
+	HangupAt             int64           `protobuf:"varint,9,opt,name=hangup_at,json=hangupAt,proto3" json:"hangup_at,omitempty"`
+	Resource             *Lookup         `protobuf:"bytes,10,opt,name=resource,proto3" json:"resource,omitempty"`
+	LegAId               string          `protobuf:"bytes,11,opt,name=leg_a_id,json=legAId,proto3" json:"leg_a_id,omitempty"`
+	LegBId               string          `protobuf:"bytes,12,opt,name=leg_b_id,json=legBId,proto3" json:"leg_b_id,omitempty"`
+	Node                 string          `protobuf:"bytes,13,opt,name=node,proto3" json:"node,omitempty"`
+	Result               string          `protobuf:"bytes,14,opt,name=result,proto3" json:"result,omitempty"`
+	Agent                *Lookup         `protobuf:"bytes,15,opt,name=agent,proto3" json:"agent,omitempty"`
+	Bucket               *Lookup         `protobuf:"bytes,16,opt,name=bucket,proto3" json:"bucket,omitempty"`
+	Logs                 *structpb.Value `protobuf:"bytes,17,opt,name=logs,proto3" json:"logs,omitempty"`
+	Active               bool            `protobuf:"varint,19,opt,name=active,proto3" json:"active,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}        `json:"-"`
+	XXX_unrecognized     []byte          `json:"-"`
+	XXX_sizecache        int32           `json:"-"`
 }
 
 func (m *MemberAttempt) Reset()         { *m = MemberAttempt{} }
@@ -1809,7 +1809,7 @@ func (m *MemberAttempt) GetBucket() *Lookup {
 	return nil
 }
 
-func (m *MemberAttempt) GetLogs() *_struct.Value {
+func (m *MemberAttempt) GetLogs() *structpb.Value {
 	if m != nil {
 		return m.Logs
 	}
--- engine/queue.pb.go
+++ engine/queue.pb.go
@@ -7,11 +7,11 @@ import (
 	context "context"
 	fmt "fmt"
 	proto "github.com/golang/protobuf/proto"
-	_struct "github.com/golang/protobuf/ptypes/struct"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	structpb "google.golang.org/protobuf/types/known/structpb"
 	math "math"
 )
 
@@ -548,7 +548,7 @@ type PatchQueueRequest struct {
 	Id                   int64             `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
 	Strategy             string            `protobuf:"bytes,2,opt,name=strategy,proto3" json:"strategy,omitempty"`
 	Enabled              bool              `protobuf:"varint,3,opt,name=enabled,proto3" json:"enabled,omitempty"`
-	Payload              *_struct.Value    `protobuf:"bytes,4,opt,name=payload,proto3" json:"payload,omitempty"`
+	Payload              *structpb.Value   `protobuf:"bytes,4,opt,name=payload,proto3" json:"payload,omitempty"`
 	Calendar             *Lookup           `protobuf:"bytes,5,opt,name=calendar,proto3" json:"calendar,omitempty"`
 	Priority             int32             `protobuf:"varint,6,opt,name=priority,proto3" json:"priority,omitempty"`
 	Name                 string            `protobuf:"bytes,7,opt,name=name,proto3" json:"name,omitempty"`
@@ -620,7 +620,7 @@ func (m *PatchQueueRequest) GetEnabled() bool {
 	return false
 }
 
-func (m *PatchQueueRequest) GetPayload() *_struct.Value {
+func (m *PatchQueueRequest) GetPayload() *structpb.Value {
 	if m != nil {
 		return m.Payload
 	}
@@ -818,7 +818,7 @@ type UpdateQueueRequest struct {
 	Id                   int64             `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
 	Strategy             string            `protobuf:"bytes,2,opt,name=strategy,proto3" json:"strategy,omitempty"`
 	Enabled              bool              `protobuf:"varint,3,opt,name=enabled,proto3" json:"enabled,omitempty"`
-	Payload              *_struct.Value    `protobuf:"bytes,4,opt,name=payload,proto3" json:"payload,omitempty"`
+	Payload              *structpb.Value   `protobuf:"bytes,4,opt,name=payload,proto3" json:"payload,omitempty"`
 	Calendar             *Lookup           `protobuf:"bytes,5,opt,name=calendar,proto3" json:"calendar,omitempty"`
 	Priority             int32             `protobuf:"varint,6,opt,name=priority,proto3" json:"priority,omitempty"`
 	Name                 string            `protobuf:"bytes,7,opt,name=name,proto3" json:"name,omitempty"`
@@ -889,7 +889,7 @@ func (m *UpdateQueueRequest) GetEnabled() bool {
 	return false
 }
 
-func (m *UpdateQueueRequest) GetPayload() *_struct.Value {
+func (m *UpdateQueueRequest) GetPayload() *structpb.Value {
 	if m != nil {
 		return m.Payload
 	}
@@ -1166,7 +1166,7 @@ func (m *SearchQueueRequest) GetType() []uint32 {
 type CreateQueueRequest struct {
 	Strategy             string            `protobuf:"bytes,1,opt,name=strategy,proto3" json:"strategy,omitempty"`
 	Enabled              bool              `protobuf:"varint,2,opt,name=enabled,proto3" json:"enabled,omitempty"`
-	Payload              *_struct.Value    `protobuf:"bytes,3,opt,name=payload,proto3" json:"payload,omitempty"`
+	Payload              *structpb.Value   `protobuf:"bytes,3,opt,name=payload,proto3" json:"payload,omitempty"`
 	Calendar             *Lookup           `protobuf:"bytes,4,opt,name=calendar,proto3" json:"calendar,omitempty"`
 	Priority             int32             `protobuf:"varint,5,opt,name=priority,proto3" json:"priority,omitempty"`
 	Name                 string            `protobuf:"bytes,6,opt,name=name,proto3" json:"name,omitempty"`
@@ -1230,7 +1230,7 @@ func (m *CreateQueueRequest) GetEnabled() bool {
 	return false
 }
 
-func (m *CreateQueueRequest) GetPayload() *_struct.Value {
+func (m *CreateQueueRequest) GetPayload() *structpb.Value {
 	if m != nil {
 		return m.Payload
 	}
@@ -1379,7 +1379,7 @@ type Queue struct {
 	UpdatedBy            *Lookup           `protobuf:"bytes,6,opt,name=updated_by,json=updatedBy,proto3" json:"updated_by,omitempty"`
 	Strategy             string            `protobuf:"bytes,7,opt,name=strategy,proto3" json:"strategy,omitempty"`
 	Enabled              bool              `protobuf:"varint,8,opt,name=enabled,proto3" json:"enabled,omitempty"`
-	Payload              *_struct.Value    `protobuf:"bytes,9,opt,name=payload,proto3" json:"payload,omitempty"`
+	Payload              *structpb.Value   `protobuf:"bytes,9,opt,name=payload,proto3" json:"payload,omitempty"`
 	Calendar             *Lookup           `protobuf:"bytes,10,opt,name=calendar,proto3" json:"calendar,omitempty"`
 	Priority             int32             `protobuf:"varint,11,opt,name=priority,proto3" json:"priority,omitempty"`
 	Name                 string            `protobuf:"bytes,12,opt,name=name,proto3" json:"name,omitempty"`
@@ -1487,7 +1487,7 @@ func (m *Queue) GetEnabled() bool {
 	return false
 }
 
-func (m *Queue) GetPayload() *_struct.Value {
+func (m *Queue) GetPayload() *structpb.Value {
 	if m != nil {
 		return m.Payload
 	}
