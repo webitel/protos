d166000d61958b7594b9df97afa4218b1cfa3d2f
navrotskyj <navrotskyj@gmail.com>
2022-02-08 13:27:27 +0200

    WTEL-2413

--- workflow/chat_connection.pb.go
+++ workflow/chat_connection.pb.go
@@ -103,21 +103,101 @@ func (m *BreakBridgeResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_BreakBridgeResponse proto.InternalMessageInfo
 
+type TransferChatPlanRequest struct {
+	ConversationId       string   `protobuf:"bytes,1,opt,name=conversation_id,json=conversationId,proto3" json:"conversation_id,omitempty"`
+	PlanId               int32    `protobuf:"varint,2,opt,name=plan_id,json=planId,proto3" json:"plan_id,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *TransferChatPlanRequest) Reset()         { *m = TransferChatPlanRequest{} }
+func (m *TransferChatPlanRequest) String() string { return proto.CompactTextString(m) }
+func (*TransferChatPlanRequest) ProtoMessage()    {}
+func (*TransferChatPlanRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_5783028bbcf301a4, []int{2}
+}
+
+func (m *TransferChatPlanRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_TransferChatPlanRequest.Unmarshal(m, b)
+}
+func (m *TransferChatPlanRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_TransferChatPlanRequest.Marshal(b, m, deterministic)
+}
+func (m *TransferChatPlanRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_TransferChatPlanRequest.Merge(m, src)
+}
+func (m *TransferChatPlanRequest) XXX_Size() int {
+	return xxx_messageInfo_TransferChatPlanRequest.Size(m)
+}
+func (m *TransferChatPlanRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_TransferChatPlanRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_TransferChatPlanRequest proto.InternalMessageInfo
+
+func (m *TransferChatPlanRequest) GetConversationId() string {
+	if m != nil {
+		return m.ConversationId
+	}
+	return ""
+}
+
+func (m *TransferChatPlanRequest) GetPlanId() int32 {
+	if m != nil {
+		return m.PlanId
+	}
+	return 0
+}
+
+type TransferChatPlanResponse struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *TransferChatPlanResponse) Reset()         { *m = TransferChatPlanResponse{} }
+func (m *TransferChatPlanResponse) String() string { return proto.CompactTextString(m) }
+func (*TransferChatPlanResponse) ProtoMessage()    {}
+func (*TransferChatPlanResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_5783028bbcf301a4, []int{3}
+}
+
+func (m *TransferChatPlanResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_TransferChatPlanResponse.Unmarshal(m, b)
+}
+func (m *TransferChatPlanResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_TransferChatPlanResponse.Marshal(b, m, deterministic)
+}
+func (m *TransferChatPlanResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_TransferChatPlanResponse.Merge(m, src)
+}
+func (m *TransferChatPlanResponse) XXX_Size() int {
+	return xxx_messageInfo_TransferChatPlanResponse.Size(m)
+}
+func (m *TransferChatPlanResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_TransferChatPlanResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_TransferChatPlanResponse proto.InternalMessageInfo
+
 type StartRequest struct {
-	ConversationId       string        `protobuf:"bytes,1,opt,name=conversation_id,json=conversationId,proto3" json:"conversation_id,omitempty"`
-	ProfileId            int64         `protobuf:"varint,2,opt,name=profile_id,json=profileId,proto3" json:"profile_id,omitempty"`
-	DomainId             int64         `protobuf:"varint,3,opt,name=domain_id,json=domainId,proto3" json:"domain_id,omitempty"`
-	Message              *chat.Message `protobuf:"bytes,4,opt,name=message,proto3" json:"message,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}      `json:"-"`
-	XXX_unrecognized     []byte        `json:"-"`
-	XXX_sizecache        int32         `json:"-"`
+	ConversationId       string            `protobuf:"bytes,1,opt,name=conversation_id,json=conversationId,proto3" json:"conversation_id,omitempty"`
+	ProfileId            int64             `protobuf:"varint,2,opt,name=profile_id,json=profileId,proto3" json:"profile_id,omitempty"`
+	DomainId             int64             `protobuf:"varint,3,opt,name=domain_id,json=domainId,proto3" json:"domain_id,omitempty"`
+	Message              *chat.Message     `protobuf:"bytes,4,opt,name=message,proto3" json:"message,omitempty"`
+	Variables            map[string]string `protobuf:"bytes,5,rep,name=variables,proto3" json:"variables,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+	SchemaId             int32             `protobuf:"varint,6,opt,name=schema_id,json=schemaId,proto3" json:"schema_id,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}          `json:"-"`
+	XXX_unrecognized     []byte            `json:"-"`
+	XXX_sizecache        int32             `json:"-"`
 }
 
 func (m *StartRequest) Reset()         { *m = StartRequest{} }
 func (m *StartRequest) String() string { return proto.CompactTextString(m) }
 func (*StartRequest) ProtoMessage()    {}
 func (*StartRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_5783028bbcf301a4, []int{2}
+	return fileDescriptor_5783028bbcf301a4, []int{4}
 }
 
 func (m *StartRequest) XXX_Unmarshal(b []byte) error {
@@ -166,6 +246,20 @@ func (m *StartRequest) GetMessage() *chat.Message {
 	return nil
 }
 
+func (m *StartRequest) GetVariables() map[string]string {
+	if m != nil {
+		return m.Variables
+	}
+	return nil
+}
+
+func (m *StartRequest) GetSchemaId() int32 {
+	if m != nil {
+		return m.SchemaId
+	}
+	return 0
+}
+
 type StartResponse struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -176,7 +270,7 @@ func (m *StartResponse) Reset()         { *m = StartResponse{} }
 func (m *StartResponse) String() string { return proto.CompactTextString(m) }
 func (*StartResponse) ProtoMessage()    {}
 func (*StartResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_5783028bbcf301a4, []int{3}
+	return fileDescriptor_5783028bbcf301a4, []int{5}
 }
 
 func (m *StartResponse) XXX_Unmarshal(b []byte) error {
@@ -199,6 +293,7 @@ var xxx_messageInfo_StartResponse proto.InternalMessageInfo
 
 type BreakRequest struct {
 	ConversationId       string   `protobuf:"bytes,1,opt,name=conversation_id,json=conversationId,proto3" json:"conversation_id,omitempty"`
+	Cause                string   `protobuf:"bytes,2,opt,name=cause,proto3" json:"cause,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -208,7 +303,7 @@ func (m *BreakRequest) Reset()         { *m = BreakRequest{} }
 func (m *BreakRequest) String() string { return proto.CompactTextString(m) }
 func (*BreakRequest) ProtoMessage()    {}
 func (*BreakRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_5783028bbcf301a4, []int{4}
+	return fileDescriptor_5783028bbcf301a4, []int{6}
 }
 
 func (m *BreakRequest) XXX_Unmarshal(b []byte) error {
@@ -236,6 +331,13 @@ func (m *BreakRequest) GetConversationId() string {
 	return ""
 }
 
+func (m *BreakRequest) GetCause() string {
+	if m != nil {
+		return m.Cause
+	}
+	return ""
+}
+
 type BreakResponse struct {
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
@@ -246,7 +348,7 @@ func (m *BreakResponse) Reset()         { *m = BreakResponse{} }
 func (m *BreakResponse) String() string { return proto.CompactTextString(m) }
 func (*BreakResponse) ProtoMessage()    {}
 func (*BreakResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_5783028bbcf301a4, []int{5}
+	return fileDescriptor_5783028bbcf301a4, []int{7}
 }
 
 func (m *BreakResponse) XXX_Unmarshal(b []byte) error {
@@ -280,7 +382,7 @@ func (m *ConfirmationMessageRequest) Reset()         { *m = ConfirmationMessageR
 func (m *ConfirmationMessageRequest) String() string { return proto.CompactTextString(m) }
 func (*ConfirmationMessageRequest) ProtoMessage()    {}
 func (*ConfirmationMessageRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_5783028bbcf301a4, []int{6}
+	return fileDescriptor_5783028bbcf301a4, []int{8}
 }
 
 func (m *ConfirmationMessageRequest) XXX_Unmarshal(b []byte) error {
@@ -332,7 +434,7 @@ func (m *ConfirmationMessageResponse) Reset()         { *m = ConfirmationMessage
 func (m *ConfirmationMessageResponse) String() string { return proto.CompactTextString(m) }
 func (*ConfirmationMessageResponse) ProtoMessage()    {}
 func (*ConfirmationMessageResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_5783028bbcf301a4, []int{7}
+	return fileDescriptor_5783028bbcf301a4, []int{9}
 }
 
 func (m *ConfirmationMessageResponse) XXX_Unmarshal(b []byte) error {
@@ -356,7 +458,10 @@ var xxx_messageInfo_ConfirmationMessageResponse proto.InternalMessageInfo
 func init() {
 	proto.RegisterType((*BreakBridgeRequest)(nil), "workflow.BreakBridgeRequest")
 	proto.RegisterType((*BreakBridgeResponse)(nil), "workflow.BreakBridgeResponse")
+	proto.RegisterType((*TransferChatPlanRequest)(nil), "workflow.TransferChatPlanRequest")
+	proto.RegisterType((*TransferChatPlanResponse)(nil), "workflow.TransferChatPlanResponse")
 	proto.RegisterType((*StartRequest)(nil), "workflow.StartRequest")
+	proto.RegisterMapType((map[string]string)(nil), "workflow.StartRequest.VariablesEntry")
 	proto.RegisterType((*StartResponse)(nil), "workflow.StartResponse")
 	proto.RegisterType((*BreakRequest)(nil), "workflow.BreakRequest")
 	proto.RegisterType((*BreakResponse)(nil), "workflow.BreakResponse")
@@ -367,34 +472,42 @@ func init() {
 func init() { proto.RegisterFile("workflow/chat_connection.proto", fileDescriptor_5783028bbcf301a4) }
 
 var fileDescriptor_5783028bbcf301a4 = []byte{
-	// 419 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x53, 0xc1, 0x6e, 0xd3, 0x40,
-	0x10, 0xad, 0x6b, 0x0a, 0xc9, 0xb4, 0xa1, 0xd2, 0x96, 0x82, 0xe5, 0x12, 0x14, 0x59, 0x45, 0x2d,
-	0x17, 0x5b, 0x2a, 0x07, 0x24, 0x8e, 0xa9, 0x84, 0x64, 0x09, 0x2e, 0xce, 0x8d, 0x4b, 0xb5, 0xf1,
-	0x4e, 0x92, 0x55, 0x6c, 0x6f, 0xd8, 0xdd, 0xc4, 0xdf, 0x82, 0xc4, 0xdf, 0xf0, 0x63, 0xc8, 0xeb,
-	0xb5, 0x71, 0x48, 0x22, 0x94, 0x4b, 0xa4, 0xbc, 0xf7, 0xe6, 0xcd, 0xcc, 0xdb, 0x31, 0xbc, 0x2b,
-	0x85, 0x5c, 0xce, 0x32, 0x51, 0x46, 0xe9, 0x82, 0xea, 0xa7, 0x54, 0x14, 0x05, 0xa6, 0x9a, 0x8b,
-	0x22, 0x5c, 0x49, 0xa1, 0x05, 0xe9, 0x35, 0xbc, 0x3f, 0xc8, 0x51, 0x29, 0x3a, 0xc7, 0x9a, 0x08,
-	0x26, 0x40, 0xc6, 0x12, 0xe9, 0x72, 0x2c, 0x39, 0x9b, 0x63, 0x82, 0x3f, 0xd6, 0xa8, 0x34, 0xb9,
-	0x83, 0xcb, 0x54, 0x14, 0x1b, 0x94, 0x8a, 0x56, 0x26, 0x4f, 0x9c, 0x79, 0xce, 0xc8, 0xb9, 0xef,
-	0x27, 0x2f, 0xbb, 0x70, 0xcc, 0xc8, 0x2b, 0x38, 0x4b, 0xe9, 0x5a, 0xa1, 0x77, 0x6a, 0xe8, 0xfa,
-	0x4f, 0x70, 0x0d, 0x57, 0x5b, 0xa6, 0x6a, 0x25, 0x0a, 0x85, 0xc1, 0x2f, 0x07, 0x2e, 0x26, 0x9a,
-	0x4a, 0x7d, 0x74, 0x9b, 0x21, 0xc0, 0x4a, 0x8a, 0x19, 0xcf, 0xb0, 0xd2, 0x54, 0xbd, 0xdc, 0xa4,
-	0x6f, 0x91, 0x98, 0x91, 0x1b, 0xe8, 0x33, 0x91, 0x53, 0x6e, 0x1c, 0x5c, 0xc3, 0xf6, 0x6a, 0x20,
-	0x66, 0xe4, 0x0e, 0x5e, 0xd8, 0x95, 0xbd, 0x67, 0x23, 0xe7, 0xfe, 0xfc, 0x61, 0x10, 0x56, 0x19,
-	0x85, 0xdf, 0x6a, 0x30, 0x69, 0xd8, 0xe0, 0x12, 0x06, 0x76, 0x3a, 0x3b, 0xef, 0x27, 0xb8, 0x30,
-	0x6b, 0x1c, 0x3b, 0x6e, 0xe5, 0x64, 0x0b, 0xad, 0xd3, 0x4f, 0x07, 0xfc, 0x47, 0x51, 0xcc, 0xb8,
-	0xcc, 0x8d, 0xa6, 0xe9, 0x7d, 0x6c, 0x0e, 0xb5, 0xb0, 0xb5, 0x69, 0xc2, 0xa8, 0x85, 0x2d, 0x1c,
-	0x33, 0xf2, 0x01, 0x7a, 0x76, 0x2d, 0xe5, 0xb9, 0x23, 0x77, 0x77, 0xeb, 0x96, 0x0e, 0x86, 0x70,
-	0xb3, 0x77, 0xb4, 0x7a, 0xf4, 0x87, 0xdf, 0xa7, 0x70, 0xfd, 0x25, 0x13, 0xe5, 0xe3, 0x82, 0xea,
-	0x09, 0xca, 0x0d, 0xca, 0xea, 0x97, 0xa7, 0x48, 0x3e, 0xc3, 0x99, 0xc9, 0x8b, 0xbc, 0x0e, 0x9b,
-	0xeb, 0x0a, 0xbb, 0xcf, 0xeb, 0xbf, 0xd9, 0xc1, 0x6d, 0x1c, 0x27, 0x55, 0xad, 0x49, 0xa8, 0x5b,
-	0xdb, 0xcd, 0xba, 0x5b, 0xbb, 0x1d, 0xe5, 0x09, 0xf9, 0x0a, 0xe7, 0x9d, 0xeb, 0x22, 0x6f, 0xff,
-	0x51, 0x6e, 0x5d, 0xb2, 0x3f, 0x3c, 0xc0, 0xb6, 0x6e, 0x0c, 0xae, 0xf6, 0xac, 0x4f, 0x6e, 0xff,
-	0xd6, 0x1d, 0x7e, 0x38, 0xff, 0xfd, 0x7f, 0x54, 0x4d, 0x97, 0xf1, 0xed, 0xf7, 0x60, 0xce, 0xf5,
-	0x62, 0x3d, 0x0d, 0x53, 0x91, 0x47, 0x25, 0x4e, 0xb9, 0xc6, 0x2c, 0x32, 0x9f, 0xa0, 0x8a, 0x1a,
-	0x8f, 0xe9, 0x73, 0x03, 0x7c, 0xfc, 0x13, 0x00, 0x00, 0xff, 0xff, 0xcc, 0xfd, 0x0c, 0xe6, 0xce,
-	0x03, 0x00, 0x00,
+	// 550 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xac, 0x54, 0xc1, 0x6e, 0xda, 0x40,
+	0x10, 0x0d, 0xb8, 0x10, 0x18, 0x42, 0x88, 0x36, 0x4d, 0xb1, 0x9c, 0x52, 0x51, 0x2b, 0x51, 0xe8,
+	0xc5, 0x48, 0xf4, 0x52, 0x45, 0x3d, 0x81, 0x5a, 0xc9, 0x52, 0x23, 0x55, 0xa6, 0xea, 0xa1, 0x39,
+	0x44, 0x8b, 0xbd, 0x80, 0x85, 0xed, 0xa5, 0xbb, 0x06, 0x94, 0x4f, 0xe9, 0xad, 0x1f, 0xd2, 0x8f,
+	0xab, 0x76, 0xbd, 0x36, 0x86, 0x40, 0x2b, 0xa4, 0x5c, 0x10, 0xfb, 0x66, 0xde, 0x9b, 0xf1, 0x7b,
+	0x5e, 0xc3, 0x9b, 0x15, 0x65, 0xb3, 0x71, 0x40, 0x57, 0x5d, 0x77, 0x8a, 0xe3, 0x07, 0x97, 0x46,
+	0x11, 0x71, 0x63, 0x9f, 0x46, 0xd6, 0x9c, 0xd1, 0x98, 0xa2, 0x4a, 0x5a, 0x37, 0xea, 0x21, 0xe1,
+	0x1c, 0x4f, 0x48, 0x52, 0x30, 0x87, 0x80, 0xfa, 0x8c, 0xe0, 0x59, 0x9f, 0xf9, 0xde, 0x84, 0x38,
+	0xe4, 0xe7, 0x82, 0xf0, 0x18, 0xdd, 0x40, 0xc3, 0xa5, 0xd1, 0x92, 0x30, 0x8e, 0x85, 0xc8, 0x83,
+	0xef, 0xe9, 0x85, 0x76, 0xa1, 0x53, 0x75, 0x4e, 0xf3, 0xb0, 0xed, 0xa1, 0x97, 0x50, 0x72, 0xf1,
+	0x82, 0x13, 0xbd, 0x28, 0xcb, 0xc9, 0xc1, 0xbc, 0x80, 0xf3, 0x0d, 0x51, 0x3e, 0xa7, 0x11, 0x27,
+	0xe6, 0x3d, 0x34, 0xbf, 0x31, 0x1c, 0xf1, 0x31, 0x61, 0x83, 0x29, 0x8e, 0xbf, 0x06, 0x38, 0x3a,
+	0x78, 0x60, 0x13, 0x8e, 0xe7, 0x01, 0x96, 0x0d, 0x62, 0x64, 0xc9, 0x29, 0x8b, 0xa3, 0xed, 0x99,
+	0x06, 0xe8, 0x4f, 0xc5, 0xd5, 0xe0, 0x3f, 0x45, 0x38, 0x19, 0xc6, 0x98, 0xc5, 0x07, 0x8f, 0x6b,
+	0x01, 0xcc, 0x19, 0x1d, 0xfb, 0x01, 0x49, 0x27, 0x6a, 0x4e, 0x55, 0x21, 0xb6, 0x87, 0x2e, 0xa1,
+	0xea, 0xd1, 0x10, 0xfb, 0x52, 0x41, 0x93, 0xd5, 0x4a, 0x02, 0xd8, 0x1e, 0xba, 0x81, 0x63, 0xe5,
+	0xb5, 0xfe, 0xa2, 0x5d, 0xe8, 0xd4, 0x7a, 0x75, 0x4b, 0x84, 0x63, 0xdd, 0x25, 0xa0, 0x93, 0x56,
+	0xd1, 0x00, 0xaa, 0x4b, 0xcc, 0x7c, 0x3c, 0x0a, 0x08, 0xd7, 0x4b, 0x6d, 0xad, 0x53, 0xeb, 0x5d,
+	0x5b, 0x69, 0x60, 0x56, 0x7e, 0x71, 0xeb, 0x7b, 0xda, 0xf7, 0x29, 0x8a, 0xd9, 0xa3, 0xb3, 0xe6,
+	0x89, 0x55, 0xb8, 0x3b, 0x25, 0x21, 0x16, 0xab, 0x94, 0xa5, 0x35, 0x95, 0x04, 0xb0, 0x3d, 0xe3,
+	0x23, 0x9c, 0x6e, 0x32, 0xd1, 0x19, 0x68, 0x33, 0xf2, 0xa8, 0x9e, 0x5a, 0xfc, 0x15, 0x51, 0x2e,
+	0x71, 0xb0, 0xc8, 0xa2, 0x94, 0x87, 0xdb, 0xe2, 0x87, 0x82, 0xd9, 0x80, 0xba, 0x5a, 0x42, 0xf9,
+	0x79, 0x07, 0x27, 0x32, 0xdf, 0x67, 0x7a, 0x5d, 0x1a, 0x50, 0x57, 0x72, 0x4a, 0xff, 0x57, 0x01,
+	0x8c, 0x01, 0x8d, 0xc6, 0x3e, 0x0b, 0x25, 0x33, 0x75, 0xec, 0xd0, 0x71, 0x49, 0x63, 0x26, 0x93,
+	0x46, 0x98, 0x34, 0x66, 0xb0, 0xed, 0xa1, 0x77, 0x50, 0x51, 0x61, 0x70, 0x5d, 0x93, 0x01, 0x6c,
+	0x65, 0x95, 0x95, 0xcd, 0x16, 0x5c, 0xee, 0x5c, 0x2d, 0x59, 0xbd, 0xf7, 0x5b, 0x83, 0x8b, 0xcf,
+	0x01, 0x5d, 0x89, 0x77, 0x70, 0x48, 0xd8, 0x92, 0x30, 0xf1, 0xeb, 0xbb, 0x04, 0xdd, 0x42, 0x49,
+	0xba, 0x88, 0x5e, 0xed, 0xce, 0xd6, 0x68, 0x3e, 0xc1, 0x95, 0x1d, 0x47, 0x82, 0x2b, 0x1d, 0xca,
+	0x73, 0xf3, 0x09, 0xe4, 0xb9, 0x9b, 0x56, 0x1e, 0xa1, 0x2f, 0x50, 0xcb, 0x5d, 0x46, 0xf4, 0x7a,
+	0xab, 0x73, 0xe3, 0xe2, 0x1b, 0xad, 0x3d, 0xd5, 0x4c, 0xcd, 0x83, 0xf3, 0x1d, 0x8f, 0x8f, 0xae,
+	0xd6, 0xbc, 0xfd, 0xc1, 0x19, 0xd7, 0xff, 0xe9, 0xca, 0xa6, 0xdc, 0xc3, 0xd9, 0xf6, 0x65, 0x46,
+	0x6f, 0xd7, 0xe4, 0x3d, 0x5f, 0x11, 0xc3, 0xfc, 0x57, 0x4b, 0x2a, 0xde, 0xbf, 0xfa, 0x61, 0x4e,
+	0xfc, 0x78, 0xba, 0x18, 0x59, 0x2e, 0x0d, 0xbb, 0x2b, 0x32, 0xf2, 0x63, 0x12, 0x74, 0xe5, 0xe7,
+	0x90, 0x77, 0x53, 0x81, 0x51, 0x59, 0x02, 0xef, 0xff, 0x06, 0x00, 0x00, 0xff, 0xff, 0x85, 0x17,
+	0xb6, 0xc1, 0x5a, 0x05, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -413,6 +526,7 @@ type FlowChatServerServiceClient interface {
 	Break(ctx context.Context, in *BreakRequest, opts ...grpc.CallOption) (*BreakResponse, error)
 	BreakBridge(ctx context.Context, in *BreakBridgeRequest, opts ...grpc.CallOption) (*BreakBridgeResponse, error)
 	ConfirmationMessage(ctx context.Context, in *ConfirmationMessageRequest, opts ...grpc.CallOption) (*ConfirmationMessageResponse, error)
+	TransferChatPlan(ctx context.Context, in *TransferChatPlanRequest, opts ...grpc.CallOption) (*TransferChatPlanResponse, error)
 }
 
 type flowChatServerServiceClient struct {
@@ -459,12 +573,22 @@ func (c *flowChatServerServiceClient) ConfirmationMessage(ctx context.Context, i
 	return out, nil
 }
 
+func (c *flowChatServerServiceClient) TransferChatPlan(ctx context.Context, in *TransferChatPlanRequest, opts ...grpc.CallOption) (*TransferChatPlanResponse, error) {
+	out := new(TransferChatPlanResponse)
+	err := c.cc.Invoke(ctx, "/workflow.FlowChatServerService/TransferChatPlan", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // FlowChatServerServiceServer is the server API for FlowChatServerService service.
 type FlowChatServerServiceServer interface {
 	Start(context.Context, *StartRequest) (*StartResponse, error)
 	Break(context.Context, *BreakRequest) (*BreakResponse, error)
 	BreakBridge(context.Context, *BreakBridgeRequest) (*BreakBridgeResponse, error)
 	ConfirmationMessage(context.Context, *ConfirmationMessageRequest) (*ConfirmationMessageResponse, error)
+	TransferChatPlan(context.Context, *TransferChatPlanRequest) (*TransferChatPlanResponse, error)
 }
 
 // UnimplementedFlowChatServerServiceServer can be embedded to have forward compatible implementations.
@@ -482,6 +606,9 @@ func (*UnimplementedFlowChatServerServiceServer) BreakBridge(ctx context.Context
 }
 func (*UnimplementedFlowChatServerServiceServer) ConfirmationMessage(ctx context.Context, req *ConfirmationMessageRequest) (*ConfirmationMessageResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ConfirmationMessage not implemented")
+}
+func (*UnimplementedFlowChatServerServiceServer) TransferChatPlan(ctx context.Context, req *TransferChatPlanRequest) (*TransferChatPlanResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method TransferChatPlan not implemented")
 }
 
 func RegisterFlowChatServerServiceServer(s *grpc.Server, srv FlowChatServerServiceServer) {
@@ -560,6 +687,24 @@ func _FlowChatServerService_ConfirmationMessage_Handler(srv interface{}, ctx con
 	return interceptor(ctx, in, info, handler)
 }
 
+func _FlowChatServerService_TransferChatPlan_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(TransferChatPlanRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FlowChatServerServiceServer).TransferChatPlan(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/workflow.FlowChatServerService/TransferChatPlan",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FlowChatServerServiceServer).TransferChatPlan(ctx, req.(*TransferChatPlanRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _FlowChatServerService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "workflow.FlowChatServerService",
 	HandlerType: (*FlowChatServerServiceServer)(nil),
@@ -579,6 +724,10 @@ var _FlowChatServerService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "ConfirmationMessage",
 			Handler:    _FlowChatServerService_ConfirmationMessage_Handler,
+		},
+		{
+			MethodName: "TransferChatPlan",
+			Handler:    _FlowChatServerService_TransferChatPlan_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
