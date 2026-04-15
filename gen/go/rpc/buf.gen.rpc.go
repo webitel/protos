95ad2858b0d6b4beda01b85cfdc609314e7b358a
srgdemon <srgdemon@gmail.com>
2026-04-15 13:11:30 +0300

    module: github.com/webitel/protos/gen/go/rpc

---
+++ gen/go/rpc/buf.gen.rpc.go
@@ -0,0 +1,3 @@
+package pbrpc
+
+//go:generate go run github.com/bufbuild/buf/cmd/buf@v1.68.1 generate --debug --template buf.gen.rpc.yaml
---
+++ gen/go/rpc/go.mod
@@ -0,0 +1,5 @@
+module github.com/webitel/protos/gen/go/rpc
+
+go 1.24.3
+
+require google.golang.org/protobuf v1.36.11
